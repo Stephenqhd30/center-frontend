79ad985968a3361feb1e9e6ef30e744c9c7de7b5
Stephenqhd30 <Popcornqhd@gmail.com>
2024-05-03 13:01:38 +0800

    init 初始化提交

---
+++ config/routes.ts
@@ -0,0 +1,33 @@
+export default [
+  { path: '/', redirect: '/welcome' },
+  { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      { name: '登录', path: '/user/login', component: './User/Login' },
+    ],
+  },
+  {
+    path: '/admin',
+    name: '管理页',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {path: '/admin', redirect: '/admin/user'},
+      { name: '用户管理', path: '/admin/user', component: './Admin/UserList' },
+    ],
+  },
+  {
+    path: '/account',
+    name: "个人页",
+    icon: "user",
+    routes: [
+      {path: '/account', redirect: '/account/center'},
+      {name: '个人中心', path: '/account/center', component: './Account/Center'},
+      {name: '个人设置', path: '/account/setting', component: './Account/Settings'},
+    ]
+
+  },
+  { path: '*', layout: false, component: './Exception/404' },
+];
